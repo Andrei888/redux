220a8e3c991732265d47733f9402b7373b50be79
Andrei Purcaru <purcaruandrei20@yahoo.com>
2022-09-30 13:41:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,23 +6,26 @@ import "dotenv/config";
 import authRouter from "./api/auth.js";
 import postsRouter from "./api/posts.js";
 import usersRouter from "./api/users.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 app.use(bodyParser.json());
 // route
 
-app.get("/", (req, res) => res.send("Hello social"));
+//app.get("/", (req, res) => res.send("Hello social"));
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postsRouter);
 app.use("/api/users", usersRouter);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    req.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
-}
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
 
 // start
 

3d6299a1acc743217ef9ab15d70c5867f2ad04dd
Andrei Purcaru <purcaruandrei20@yahoo.com>
2022-10-14 03:53:13 +0300

    up

--- client/src/actions/pushmovie.js
+++ client/src/actions/pushmovie.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 import { setAlert } from "./alerts";
+import store from "../store";
 
 const pushmovie = async ({ title, text }) => {
   console.log("sxxxbt");
+  const token = store.getState().auth.token;
   const config = {
     headers: {
       "Content-Type": "application/json",
+      "x-auth-token": token ? token : "",
     },
   };
   const movie = {
@@ -15,7 +18,7 @@ const pushmovie = async ({ title, text }) => {
   const body = JSON.stringify(movie);
   console.log("sbt", movie);
   try {
-    const response = await axios.put("/api/posts", body, config);
+    const response = await axios.post("/api/posts", body, config);
     console.log(response);
     setAlert("Film Adaugat", "danger", 3000);
   } catch (error) {

cad233c8da661ffb8e0b329cd262ad03b8c2768a
Andrei Purcaru <purcaruandrei20@yahoo.com>
2022-10-18 18:05:33 +0300

    Update pushmovie.js

--- client/src/actions/pushmovie.js
+++ client/src/actions/pushmovie.js
@@ -3,37 +3,31 @@ import { setAlert } from "./alerts";
 import store from "../store";
 import { ADD_MOVIE } from "./constants";
 
-export const pushmovie =
-  ({ title, text }) =>
-  async (dispatch) => {
-    console.log("sxxxbt");
-    const token = store.getState().auth.token;
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-        "x-auth-token": token ? token : "",
-      },
-    };
-    const movie = {
-      title,
-      text,
-    };
-    const body = JSON.stringify(movie);
-    console.log("sbt", movie);
-    try {
-      const response = await axios.post("/api/posts", body, config);
-      console.log(response);
-
-      dispatch({
-        type: ADD_MOVIE,
-        payload: response.data,
-      });
-      setAlert("Film Adaugat", "danger", 3000);
-    } catch (error) {
-      console.log(error);
-      const errors = error.response.data.errors;
-
-      if (errors) {
-      }
-    }
+export const pushmovie = async ({ title, text }) => {
+  console.log("sxxxbt");
+  const token = store.getState().auth.token;
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      "x-auth-token": token ? token : "",
+    },
   };
+  const movie = {
+    title,
+    text,
+  };
+  const body = JSON.stringify(movie);
+  console.log("sbt", movie);
+  try {
+    const response = await axios.post("/api/posts", body, config);
+    console.log(response);
+
+    setAlert("Film Adaugat", "danger", 3000);
+  } catch (error) {
+    console.log(error);
+    const errors = error.response.data.errors;
+
+    if (errors) {
+    }
+  }
+};

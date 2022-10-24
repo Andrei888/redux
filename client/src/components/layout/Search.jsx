f567b13735a03d18b2796dc73ca9ec9d4497b2d8
Andrei Purcaru <purcaruandrei20@yahoo.com>
2022-10-24 21:06:14 +0300

    up

--- client/src/components/layout/Search.jsx
+++ client/src/components/layout/Search.jsx
@@ -4,39 +4,52 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 function Search() {
-  const [searchList, setSearchList] = useState();
+  const [searchList, setSearchList] = useState([]);
   async function fetchMovies() {
     const res = await axios.get("/api/posts");
-    setSearchList(res.data);
+    const data = res.data;
+    setSearchList(data);
     console.log(searchList);
   }
   useEffect(() => {
     fetchMovies();
   }, []);
-  const searchHandler = (e) => {
+  const searchToggle = (e) => {
     e.preventDefault();
-    if (e.target.parentElement.parentElement.classList.contains("js--open")) {
-      const value = e.target.value;
-      if (searchList && value.length > 2) {
-        searchList.filter((movie) => {
-          console.log(movie);
-          return movie.indexOf(value) > -1 ? true : false;
-        });
-      }
+    if (e.currentTarget.parentElement.classList.contains("js--open")) {
+      e.currentTarget.parentElement.classList.remove("js--open");
     } else {
-      e.target.parentElement.parentElement.classList.add("js--open");
+      e.currentTarget.parentElement.classList.add("js--open");
+    }
+  };
+  const searchHandler = (e) => {
+    const searchInput = e.target;
+    const value = searchInput.value;
+    console.log(value);
+    if (searchList && value.length > 2) {
+      console.log(searchList);
+      searchList.filter((movie) => {
+        console.log(movie);
+        return movie.indexOf(value) > -1 ? true : false;
+      });
     }
   };
   return (
     <div className="module-search p-2">
       <div className="module-search_input-container">
         <input
+          id="search-value"
           className="module-search_input"
           type="text"
           placeholder="Cauta film"
+          onChange={(e) => searchHandler(e)}
         />
       </div>
-      <button className="module-search-btn" onClick={(e) => searchHandler(e)}>
+      <button
+        className="module-search-btn p-2"
+        id="search-btn"
+        onClick={(e) => searchToggle(e)}
+      >
         <FontAwesomeIcon icon={faMagnifyingGlass} />
       </button>
       <div className="module-search_results"></div>
--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -23,7 +23,6 @@ const Posts = ({ setAlert }) => {
     }
   }
   useEffect(() => {
-    console.log("effect-postss");
     fetchData();
   }, []);
   return (
